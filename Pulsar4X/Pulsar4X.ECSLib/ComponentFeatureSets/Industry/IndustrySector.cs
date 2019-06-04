b1ef21466a082cebc96ee7c35855f96b5ff6c34e
UberWaffe@gmail.com <UberWaffe@gmail.com>
2019-06-04 14:33:11 +0200

    Added first test for Industrial Economy.

---
+++ Pulsar4X/Pulsar4X.ECSLib/ComponentFeatureSets/Industry/IndustrySector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace Pulsar4X.ECSLib
+{
+    public class IndustrySector
+    {
+        private IndustrySD _data;
+
+        public IndustrySector(IndustrySD instance)
+        {
+            _data = instance;
+        }
+
+        public BatchTradeGoods ProductionResult()
+        {
+            return _data.Output;
+        }
+    }
+
+
+}
---
+++ Pulsar4X/Pulsar4X.ECSLib/StaticData/Industry/BatchTradeGoods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pulsar4X.ECSLib
+{
+    public class BatchTradeGoods
+    {
+        public Dictionary<Guid, long> FullItems { get; } = new Dictionary<Guid, long>();
+
+        public BatchTradeGoods()
+        {
+            FullItems = new Dictionary<Guid, long>();
+        }
+
+        public void AddTradeGood(TradeGoodSD theGood, long amount)
+        {
+            FullItems[theGood.ID] += Math.Abs(amount);
+        }
+    }
+}
