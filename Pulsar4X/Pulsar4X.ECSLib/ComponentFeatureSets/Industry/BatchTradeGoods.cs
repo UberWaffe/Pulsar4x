d1f8b1fa82956458a75dd551b6277648a414c44b
UberWaffe@gmail.com <UberWaffe@gmail.com>
2019-06-13 23:03:14 +0200

    Add services to industrial economy.
    - Servies are like cargo but take no space, have no mass, and cannot be stockpiled at all (i.e. they are either consumed immediately or are loss).
    - Idea is that services must be available locally, and cannot be traded / shipped around.

---
+++ Pulsar4X/Pulsar4X.ECSLib/ComponentFeatureSets/Industry/BatchTradeGoods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pulsar4X.ECSLib
+{
+    public class BatchTradeGoods
+    {
+        public Dictionary<Guid, long> Items { get; private set;  } = new Dictionary<Guid, long>();
+        public Dictionary<Guid, long> Services { get; private set; } = new Dictionary<Guid, long>();
+
+        public BatchTradeGoods()
+        {
+            Items = new Dictionary<Guid, long>();
+            Services = new Dictionary<Guid, long>();
+        }
+
+        public BatchTradeGoods(BatchTradeGoods batchToCopy)
+        {
+            Items = new Dictionary<Guid, long>();
+            Services = new Dictionary<Guid, long>();
+
+            foreach (var entry in batchToCopy.Items)
+            {
+                Items.Add(entry.Key, entry.Value);
+            }
+            foreach (var entry in batchToCopy.Services)
+            {
+                Services.Add(entry.Key, entry.Value);
+            }
+        }
+
+        public void AddTradeGood(TradeGoodSD theGood, int amount)
+        {
+            if (Items.ContainsKey(theGood.ID) == false)
+            {
+                Items[theGood.ID] = 0;
+            }
+
+            Items[theGood.ID] += Math.Abs(amount);
+        }
+
+        public void ChangeService(EconomyServiceSD theService, int amount)
+        {
+            if (Services.ContainsKey(theService.ID) == false)
+            {
+                Services[theService.ID] = 0;
+            }
+
+            Services[theService.ID] += amount;
+            Services[theService.ID] = Math.Max(Services[theService.ID], 0);
+        }
+
+        public void MultiplyDivideBy(long multiplier = 1, long divider = 1)
+        {
+            var updatedDictionary = new Dictionary<Guid, long>();
+            foreach (var entry in Items)
+            {
+                var result = entry.Value;
+                result *= multiplier;
+                result /= divider;
+                updatedDictionary[entry.Key] = result;
+            }
+            Items = updatedDictionary;
+
+            updatedDictionary = new Dictionary<Guid, long>();
+            foreach (var entry in Services)
+            {
+                var result = entry.Value;
+                result *= multiplier;
+                result /= divider;
+                updatedDictionary[entry.Key] = result;
+            }
+            Services = updatedDictionary;
+        }
+    }
+}
---
+++ Pulsar4X/Pulsar4X.ECSLib/ComponentFeatureSets/Industry/CargoAndServices.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pulsar4X.ECSLib
+{
+    public class CargoAndServices
+    {
+        #region Properties
+        public CargoStorageDB AvailableCargo { get; private set;  } = new CargoStorageDB();
+        public Dictionary<Guid, long> AvailableServices { get; private set; } = new Dictionary<Guid, long>();
+
+        private TradeGoodLibrary _tradeGoodsLibrary;
+        #endregion
+
+        #region Constructors
+        public CargoAndServices(TradeGoodLibrary tradeGoodsLibrary)
+            : this(new CargoStorageDB(), new Dictionary<Guid, long>(), tradeGoodsLibrary)
+        {
+            
+        }
+
+        public CargoAndServices(CargoStorageDB currentCargo, Dictionary<Guid, long> currentAvailableServices, TradeGoodLibrary tradeGoodsLibrary)
+        {
+            AvailableCargo = currentCargo;
+            AvailableServices = currentAvailableServices;
+            _tradeGoodsLibrary = tradeGoodsLibrary;
+        }
+        #endregion
+
+        public long CalculateMaximumBatchesPossibleWithAvailableInputs(BatchTradeGoods consumptionRequirementsOfOneBatch)
+        {
+            var finalBatchesCount = long.MaxValue;
+            
+            foreach (var goodEntry in consumptionRequirementsOfOneBatch.Items)
+            {
+                var good = _tradeGoodsLibrary.Get(goodEntry.Key);
+                var amountNeeded = goodEntry.Value;
+
+                var stockpiledAmount = StorageSpaceProcessor.GetAmount(AvailableCargo, good.CargoTypeID, good.ID);
+                var countOfBatchesWorthOfStockpile = stockpiledAmount / amountNeeded;
+
+                finalBatchesCount = Math.Min(finalBatchesCount, countOfBatchesWorthOfStockpile);
+
+                if (finalBatchesCount <= 0)
+                    return 0;
+            }
+
+            foreach (var serviceEntry in consumptionRequirementsOfOneBatch.Services)
+            {
+                var amountNeeded = serviceEntry.Value;
+
+                var availableAmount = AvailableServices[serviceEntry.Key];
+                var countOfBatchesWorthOfService = availableAmount / amountNeeded;
+
+                finalBatchesCount = Math.Min(finalBatchesCount, countOfBatchesWorthOfService);
+
+                if (finalBatchesCount <= 0)
+                    return 0;
+            }
+
+            return finalBatchesCount;
+        }
+
+        public long CalculateMaximumBatchesPossibleWithOutputStorage(BatchTradeGoods productionResultsOfOneBatch)
+        {
+            var finalBatchesCount = long.MaxValue;
+            
+            foreach (var goodEntry in productionResultsOfOneBatch.Items)
+            {
+                var good = _tradeGoodsLibrary.Get(goodEntry.Key);
+                var spaceNeededForThisGoodForOneBatch = goodEntry.Value;
+
+                var freeCapacity = StorageSpaceProcessor.GetFreeCapacity(AvailableCargo, good);
+                var countOfBatchesWorthOfSpace = freeCapacity / spaceNeededForThisGoodForOneBatch;
+
+                finalBatchesCount = Math.Min(finalBatchesCount, countOfBatchesWorthOfSpace);
+            }
+
+            return finalBatchesCount;
+        }
+
+        public long ChangeService(Guid service, long amount)
+        {
+            AvailableServices.SafeValueAdd(service, amount);
+            return AvailableServices[service];
+        }
+
+    }
+}
